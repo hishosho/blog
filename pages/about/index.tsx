2eebe7c93265e1fb67034849593f2814f847fb2e
zhangshuo <zhangs001@cmft.com>
2021-09-28 23:09:22 +0800

    [Feat]: 添加关于板块中的导航

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,40 +1,171 @@
 import Image from 'next/image'
 import photo from '../../public/photo.jpg'
 import styles from '../../styles/About.module.css'
+import { useState } from 'react'
+import Router from 'next/router'
 const About = () => {
-  return (
-    <div className={styles.aboutWraper}>
+  const [onNavigation, setOnNavigation] = useState<boolean>(false)
+  const skillList = [
+    {
+      id: 'HTML',
+      name: 'HTML',
+      backgroundColor: '#ede574',
+      items: ['HTML5', 'Canvas', 'SVG'],
+    },
+    {
+      id: 'CSS',
+      name: 'CSS',
+      backgroundColor: '#f9d423',
+      items: ['CSS3', 'Less', 'Sass']
+    },
+    {
+      id: 'JavaScript',
+      name: 'JavaScript',
+      backgroundColor: '#fc913a',
+      items: ['JavaScript', 'ES6', 'TypeScript']
+    },
+    {
+      id: 'Vue',
+      name: 'Vue',
+      backgroundColor: '#ff4e50',
+      items: ['Vue', 'Vue Router', 'Vuex']
+    },
+    {
+      id: 'React',
+      name: 'React',
+      backgroundColor: '#ff7761',
+      items: ['React hook', 'React Router', 'Redux']
+    },
+    {
+      id: 'Tools',
+      name: 'Tools',
+      backgroundColor: '#fd8f8f',
+      items: ['webpack', 'Negix', 'Git']
+    }
+  ]
+  const background = () => {
+    const bgStyles = [ 'rightLight', 'rightDark', 'leftLight', 'leftDark' ]
+    return (
       <aside className={styles.backgroundAside}>
-        <div className={styles.rightFirst}></div>
-        <div className={styles.rightSecond}></div>
-        <div className={styles.leftFirst}></div>
-        <div className={styles.leftSecond}></div>
+        {
+          bgStyles.map((itemStyle) => <div key={itemStyle} className={styles[itemStyle]}></div>)
+        }
       </aside>
+    )
+  }
+  const authorPhoto = () => {
+    return (
+      <div className={styles.photo}>
+        <Image src={photo} alt="author"></Image>
+      </div>
+    )
+  }
+  const introduceContent = () => {
+    return (
+      <div className={styles.introduceContent}>
+        <div className={styles.title}>Hello!</div>
+        <div>
+          <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
+          <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
+          <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
+          <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
+          <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
+        </div>
+      </div>
+    )
+  }
+  const main = () => {
+    return (
       <main className={styles.main}>
         <section className={styles.introduce}>
-          <div className={styles.photo}>
-            <Image src={photo} alt="author"></Image>
-          </div>
-          <div className={styles.introduceContent}>
-            <div className={styles.title}>Hello!</div>
-            <div>
-              <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
-              <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
-              <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
-              <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
-              <p>萨达萨达萨达萨达萨达萨达萨达萨达萨达，萨达萨达萨达萨达。</p>
-            </div>
-          </div>
+          {authorPhoto()}
+          {introduceContent()}
         </section>
       </main>
+    )
+  }
+  const skills = () => {
+    const itemStyle = {
+      width: `calc((100vw - 20%) / ${skillList.length})`,
+      height: `calc(100vh / ${skillList.length + 1})`
+    }
+    return (
       <section className={styles.footer}>
-        <div className={styles.more} style={{background: '#ede574'}}><span>HTML</span></div>
-        <div className={styles.more} style={{background: '#f9d423'}}><span>CSS</span></div>
-        <div className={styles.more} style={{background: '#fc913a'}}><span>JS</span></div>
-        <div className={styles.more} style={{background: '#ff4e50'}}><span>Vue</span></div>
-        <div className={styles.more} style={{background: '#ff7761'}}><span>React</span></div>
-        <div className={styles.more} style={{background: '#fd8f8f'}}><span>Tools</span></div>
+        {
+          skillList.map(skill => (
+            <div 
+              key={skill.id}
+              className={styles.more}
+              style={{background: skill.backgroundColor, ...itemStyle}}
+            >
+              <p>
+                {skill.name}
+              </p>
+              <p 
+                className={styles.skillItems}
+              >
+                { skill.items.map(item =><span key={item}>{item}</span>)}
+              </p>
+            </div>
+          ))
+        }
       </section>
+    )
+  }
+  const smallNav = () => {
+    const enterNavigation = (state: boolean) => {
+      setOnNavigation(state)
+    }
+    const navigations = [
+      {
+        id: 'home',
+        name: 'Home',
+        path: '/'
+      },
+      {
+        id: 'blog',
+        name: 'blog',
+        path: '/blog'
+      },
+      {
+        id: 'about',
+        name: 'About',
+        path: '/about'
+      }
+    ]
+    
+    return (
+      <div
+        className={styles.navigation}
+        onMouseEnter={() => enterNavigation(true)}
+        onMouseLeave={() => enterNavigation(false)}
+      >
+        <ul
+          className={styles.navigationList}
+        >
+          <li className={styles.navigationItem}>Menu</li>
+          {
+            navigations.map((nav, i) => (
+              <li 
+                key={nav.id}
+                className={`${styles.navigationItem} ${onNavigation ? styles.navActive : styles.navUnActive}`}
+                style={{transform: onNavigation===false ? `translate(${100 * i}%)` : '', transitionDelay: `.${i}s`, cursor: 'pointer'}}
+                onClick={() => Router.push(nav.path)}
+              >
+                {nav.name}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+  return (
+    <div className={styles.aboutWrapper}>
+      {smallNav()}
+      {background()}
+      {main()}
+      {skills()}
     </div>
   )
 }
